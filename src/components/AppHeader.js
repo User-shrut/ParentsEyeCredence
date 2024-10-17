6fd2c9421055751da785d7ccae297e31ca17270e
AlbakhshSheikh <albakhshsheikhofficial@gmail.com>
2024-10-17 17:04:21 +0530

    dark_mode

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -140,7 +140,7 @@ const AppHeader = () => {
   const decodedToken = token ? jwtDecode(token) : null;
   const socket = io(`${import.meta.env.VITE_API_URL}`);
   const userId = decodedToken && decodedToken.id;
-  const [notifications , setNotifications] = useState([]);
+  const [notifications, setNotifications] = useState([]);
 
   const notificationSocket = () => {
     const audio = new Audio(notificationSound);
@@ -156,7 +156,7 @@ const AppHeader = () => {
   useEffect(() => {
     console.log("this is notification socket code");
     notificationSocket();
-    
+
   }, [])
 
   // #########################################################
@@ -262,6 +262,49 @@ const AppHeader = () => {
           <NotificationDropdown notifications={notifications} />
         </CHeaderNav>
         <CHeaderNav>
+          <li className="nav-item py-1">
+            <div className="vr h-100 mx-2 text-body text-opacity-75"></div>
+          </li>
+          <CDropdown variant="nav-item" placement="bottom-end">
+            <CDropdownToggle caret={false}>
+              {colorMode === 'dark' ? (
+                <CIcon icon={cilMoon} size="lg" />
+              ) : colorMode === 'auto' ? (
+                <CIcon icon={cilContrast} size="lg" />
+              ) : (
+                <CIcon icon={cilSun} size="lg" />
+              )}
+            </CDropdownToggle>
+            <CDropdownMenu>
+              <CDropdownItem
+                active={colorMode === 'light'}
+                className="d-flex align-items-center"
+                as="button"
+                type="button"
+                onClick={() => setColorMode('light')}
+              >
+                <CIcon className="me-2" icon={cilSun} size="lg" /> Light
+              </CDropdownItem>
+              <CDropdownItem
+                active={colorMode === 'dark'}
+                className="d-flex align-items-center"
+                as="button"
+                type="button"
+                onClick={() => setColorMode('dark')}
+              >
+                <CIcon className="me-2" icon={cilMoon} size="lg" /> Dark
+              </CDropdownItem>
+              <CDropdownItem
+                active={colorMode === 'auto'}
+                className="d-flex align-items-center"
+                as="button"
+                type="button"
+                onClick={() => setColorMode('auto')}
+              >
+                <CIcon className="me-2" icon={cilContrast} size="lg" /> Auto
+              </CDropdownItem>
+            </CDropdownMenu>
+          </CDropdown>
 
           <li className="nav-item py-1">
             <div className="vr h-100 mx-2 text-body text-opacity-75"></div>
