8c8ad8dcf3e8d31836124f25ee96666ae9648462
Yash Dahiwale <yashdahiwale3@gmail.com>
2024-11-07 11:04:42 +0530

    added credence loading logo

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './Loader.css'
+import loader from '../../assets/credenceLoader/Creadence_logo_png.png'
 
 const Loader = () => {
   return (
     <div className="loaderContainer">
-      <div className="loader"></div>
+      <img className='loader' src={loader} alt="Loading..." />
+      Loading...
     </div>
   )
 }
